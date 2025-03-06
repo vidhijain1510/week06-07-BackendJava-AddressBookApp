9b1aca0caecada16deed4ebf7966af3c9431a7c3
Vidhi Jain <vidhijain507@gmail.com>
2025-03-06 12:50:44 +0530

    [Vidhi_Jain]Add. Using Lombok Library for Logging.

--- src/main/java/com/capgemini/addressbookapp/AddressBookAppApplication.java
+++ src/main/java/com/capgemini/addressbookapp/AddressBookAppApplication.java
@@ -1,13 +1,17 @@
 package com.capgemini.addressbookapp;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class AddressBookAppApplication {
 
 	public static void main(String[] args) {
+		log.info("Starting Address Book Application...");
 		SpringApplication.run(AddressBookAppApplication.class, args);
+		log.info("Address Book Application started successfully.");
 	}
-
 }
+
